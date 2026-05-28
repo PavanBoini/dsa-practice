b000c7b724d99640ceed487acd4f18007d51df2a
PavanBoini <boinipavankumar86@gmail.com>
2026-05-28 22:41:40 +0530

    Hello World

---
+++ arrays/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String[] greetings = {"Hello", "Hi", "Hey", "Welcome"};
+        
+        for (String greeting : greetings) {
+            System.out.println(greeting + ", World!");
+        }
+    }
+}
